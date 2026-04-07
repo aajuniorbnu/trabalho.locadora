a3f6ee666d39a961d819d15ee6b6898d5dcb1b71
vinicius imhof waldrigues <vinicius.waldrigues@unifebe.edu.br>
2026-04-07 18:56:29 -0300

    alterações na parte de js da tela de login

--- js/login.js
+++ js/login.js
@@ -13,6 +13,8 @@ function entrar() {
     if (senha === "12345" && email === "batatinha@gmail.com") {
         alert("Login efetuado com sucesso");
         window.location.href = 'veiculos.html';
+        document.getElementById("fazer-login").style.display = "none"
+        document.getElementById("login-feito").style.display = "flex"
     } else {
         alert("Email ou senha incorretos!");
     }
