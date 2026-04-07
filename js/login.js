6c2d5caafcfce1e46d24b946464a7ed26457ce26
vinicius imhof waldrigues <vinicius.waldrigues@unifebe.edu.br>
2026-04-07 16:27:41 -0300

    parte do js da tela de login

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const campoEmail = document.getElementById("email");
+
+const campoSenha = document.getElementById("senha");
+
+const botaoLogin = document.getElementById("entrar");
+botaoLogin.addEventListener("click", entrar);
+
+function entrar() {
+
+    const email = campoEmail.value;
+    const senha = campoSenha.value;
+
+    if (senha === "12345" && email === "batatinha@gmail.com") {
+        alert("Login efetuado com sucesso");
+        window.location.href = 'veiculos.html';
+    } else {
+        alert("Email ou senha incorretos!");
+    }
+
+}
